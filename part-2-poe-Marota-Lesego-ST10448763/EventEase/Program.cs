633fb3b7f319f39940fd79e2c60fec01ded4a1ee
Lesego Marota <164771272+Marota-Lesego-ST10448763@users.noreply.github.com>
2025-06-25 00:36:09 +0200

    Third Commit
    Third Commit

--- part-2-poe-Marota-Lesego-ST10448763/EventEase/Program.cs
+++ part-2-poe-Marota-Lesego-ST10448763/EventEase/Program.cs
@@ -1,24 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using EventEase.Models;
-using EventEase.Services; // Include this to access IAzureBlobStorageService
+using EventEase.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Hardcoded connection string 
-var connectionString = "Server=tcp:eventeasebooking.database.windows.net,1433;Initial Catalog=EventEaseBookingDB;Persist Security Info=False;User ID=LesegoMarota;Password=BengandRama*1738;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+// Hardcoded connection string (Make sure this is safe in your environment)
+var connectionString = "Server=tcp:eventeasebookingsv.database.windows.net,1433;Initial Catalog=EventEaseSystemDB;Persist Security Info=False;User ID=LesegoRMarota;Password=BengandRama*1738;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-// Add services
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configure DbContext with EnableRetryOnFailure for transient fault handling
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, sqlOptions =>
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5, // Number of retries before failing
+            maxRetryDelay: TimeSpan.FromSeconds(30), // Max delay between retries
+            errorNumbersToAdd: null // Use default transient errors
+        )
+    )
+);
 
 // Register Azure Blob Storage service
 builder.Services.AddScoped<IAzureBlobStorage, AzureBlobStorage>();
 
 var app = builder.Build();
 
-// Configure middleware
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -31,6 +39,7 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
 
 app.MapControllerRoute(
